a4827d9b188a438f0ea6068119c8dd86e28ca188
Benjamin Sanhueza A. <bisalva99@gmail.com>
2025-08-21 17:50:47 -0300

    excercise #3 oop (#12)
    * feat : add #3 excercise oop

---
+++ 2-oop-exercises/3-excercise.py
@@ -0,0 +1,48 @@
+
+class Employee:
+    def __init__(self,name,last_name,social_number,initial_salary=0):
+        self.name = name
+        self.last_name = last_name
+        self.social_number = social_number
+        self.salary = initial_salary
+
+    def calculate_salary(self):
+        return self.salary
+    
+    def __str__(self):
+        return f"Employe details - Name : {self.name} ; Last Name : {self.last_name} ; Social Number : {self.social_number} ; Salary : {self.salary}"
+
+class Seller(Employee):
+    def __init__(self,name,last_name,social_number,initial_salary,commissions=None):
+        super().__init__(name,last_name,social_number,initial_salary)
+
+        if commissions is None:
+            self.commissions = []
+        else:
+            self.commissions = commissions
+
+    def add_commission(self,amount):
+        self.commissions.append(amount)
+
+    def calculate_salary(self):
+        total_commissions = sum(self.commissions)
+        self.salary += total_commissions
+        return self.salary
+
+
+class Manager(Employee):
+    def __init__(self,name,last_name,social_number,initial_salary,manager_bonus):
+        super().__init__(name,last_name,social_number,initial_salary)
+        self.manager_bonus = manager_bonus
+
+    def calculate_salary(self):
+        self.salary = self.salary + (self.salary * (self.manager_bonus/100))
+        return self.salary
+
+seller = Seller("Juan", "Pérez", "12345678-9", 500000)
+seller.add_commission(50000)
+seller.add_commission(75000)
+print(f"Seller salary : {seller.calculate_salary()}")
+
+manager = Manager("María", "González", "98765432-1", 800000, 25)
+print(f"Manager salary: {manager.calculate_salary()}")
